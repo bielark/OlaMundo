cc7f7b3bc65539712765fcffa16009ac79fc58f9
gabriel brites <gabrielark97@gmail.com>
2023-08-01 09:20:29 -0300

    projeto coom variavel com numeros inteiros e reais

--- variavel.py
+++ variavel.py
@@ -2,3 +2,72 @@ nome = input("qual e seu nome?")
 
 print("ola,",nome, ".tudo bem com vc?")
 print("alteraçao depois de clonado")
+
+print(type(nome))
+a = 10
+b = 5.8
+c = "Rio de janeiro"
+d = True
+
+print("a: ",a)
+print("b: ",b)
+print("c: ",c)
+print("d: ",d)
+
+
+print("tipo da var a: ",type(a)) # tipo inteiro (numro inteiro)
+print("tipo da var b: ",type(b)) # tipo float (numero reais)
+print("tipo da var c: ",type(c)) # tipo string (alfanumerico)
+print("tipo da var d: ",type(d)) # tipo boolean (true ou false)
+
+a = 20
+print("a: ",a)
+b = "sao paulo "
+print("b: ",b)
+
+print("tipo da var a: ",type(a)) # tipo inteiro (numro inteiro)
+print("tipo da var b: ",type(b)) # tipo float (numero reais)
+
+a = input("infomer um numero")
+b = input("infomer outro numero")
+print("a: ",a, "- b =",b)
+print("tipo da var a: ",type(a))
+print("tipo da var b: ",type(b))
+
+c = a + b # concatenou as strings de a e b
+print("c: ",c)
+print("tipo da var c: ",type(c))
+
+d = int(a)
+print("d: ",d)
+print("tipo da var d: ",type(d))
+
+a = int(input("infomer um numero"))
+b = int(input("infomer outro numero"))
+print("a: ",a, "- b =",b)
+print("tipo da var a: ",type(a))
+print("tipo da var b: ",type(b))
+
+c = a + b
+print("c: ",c)
+print("tipo da var c: ",type(c))
+
+a = float(input("infomer um numero"))
+b = float(input("infomer outro numero"))
+print("a: ",a, "- b =",b)
+print("tipo da var a: ",type(a))
+print("tipo da var b: ",type(b))
+
+c = a + b
+print("c: ",c)
+print("tipo da var c: ",type(c))
+
+
+
+
+
+
+
+
+
+
