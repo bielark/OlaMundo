d10bd4d89054c3fa84dd22c9a40a21d64d4bd05c
gabriel brites <gabrielark97@gmail.com>
2023-08-01 09:59:05 -0300

    exercicio com variavel

---
+++ exercicio1.py
@@ -0,0 +1,10 @@
+# parte 1
+a = float(input("digite a sua primeira nota "))
+b = float(input("segunda "))
+c = float(input("e a ultima"))
+#parte 2
+d = a + b + c
+print("total: ",d)
+#parte 3
+e = d / 3
+print("media : ",e)
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+a = int(input("digite um numero inteiro "))
+
+b = a +1
+c = a - 1
+print("anterior:",c, "sucessor :",b)
